72a862084d3b0c2018b6df09718bff8d0ab28369
邱驰艺 <qiuchiyi@ZBMAC-C02VV0Z8H.local>
2019-05-28 15:44:06 +0800

    框架搭建 | svg 脚本优化

--- scripts/template/svg.ts
+++ scripts/template/svg.ts
@@ -1,10 +1,10 @@
 'use strict';
 import fs from 'fs';
 import SVGO from 'svgo';
-import { getProjectUrl, EOL } from '../helpers';
+import { getProjectUrl } from '../helpers';
 
-const dirList = ['src', 'Icon', 'svgs'];
-const dirPath = getProjectUrl(...dirList),
+const dirList = ['src', 'Icon'];
+const dirPath = getProjectUrl(...dirList, 'svgs'),
     // https://github.com/svg/svgo
     svgo = new SVGO({
         plugins: [
@@ -124,39 +124,47 @@ const svgName: string[] = [];
 
 let str = `const svgs = {`;
 
+// let kk = 0;
 async function watiSvgo(key: string, data: string, filePath: string): Promise<void> {
     const result = await svgo.optimize(data, { path: filePath });
-    if (key.split('.')[0]) {
-        svgName.push(key.split('.')[0]);
-        str += key.split('.')[0] + ":'" + result.data + "',";
-    }
+    svgName.push(key.split('.')[0]);
+    str += key.split('.')[0] + ":'" + result.data + "',";
 }
-
 Promise.all(
     svgList.map(async key => {
+        if (key.split('.')[1] !== 'svg') {
+            return Promise.resolve();
+        }
         try {
-            const filePath = getProjectUrl(...dirList, key);
-            const data = fs.readFileSync(filePath, 'utf8');
-            return await watiSvgo(key, data, filePath);
+            const filePath = getProjectUrl(...dirList, 'svgs', key);
+
+            let data = fs.readFileSync(filePath); //通过node 中fs模块同步读文件内容
+            //判断文件头的字节
+            if (data[0] === 0xef && data[1] === 0xbb && data[2] === 0xbf) {
+                data = data.slice(3);
+            }
+            return await watiSvgo(key, data.toString('utf8'), filePath);
         } catch (err) {
-            console.log(err);
+            console.log('--err--', err);
         }
     }),
 )
     .then(() => {
         str += '};export default svgs;';
-        fs.writeFileSync(getProjectUrl(...dirList, '../', 'svgs.tsx'), str, 'utf8');
+        fs.writeFileSync(getProjectUrl(...dirList, 'svgs.tsx'), str, 'utf8');
     })
     .then(() => {
-        let propType = fs.readFileSync(getProjectUrl(...dirList, '../', 'PropsType.tsx'), 'utf8');
-        // svgName.push('string');
-        const str = propType
-            .split(EOL)
-            .map(item => {
-                return item.replace(/value: .*;/, `value: '${svgName.join("' | '")}' | string;`);
-            })
-            .join(EOL);
-        fs.writeFileSync(getProjectUrl(...dirList, '../', 'PropsType.tsx'), str, 'utf8');
+        let str = fs.readFileSync(getProjectUrl(...dirList, 'PropsType.tsx'), 'utf8');
+        const propsStr = str.replace(/value:(.|\n)*?;/, `value: '${svgName.join("' | '")}';`);
+        fs.writeFileSync(getProjectUrl(...dirList, 'PropsType.tsx'), propsStr, 'utf8');
+    })
+    .then(() => {
+        let str = fs.readFileSync(getProjectUrl(...dirList, 'demo', 'index.tsx'), 'utf8');
+        const propsStr = str.replace(
+            /(?<=IconProps\['value'\]\[\] = \[)(.|\n)*?(?=\];)/,
+            svgName.map(item => `'${item}'`).join(','),
+        );
+        fs.writeFileSync(getProjectUrl(...dirList, 'demo', 'index.tsx'), propsStr, 'utf8');
         console.log('> Congratulations, generator svgs success !!!');
     })
     .catch(err => console.log(err));
